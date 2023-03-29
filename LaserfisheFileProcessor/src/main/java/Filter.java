ad5bd468a801b7af3de8c0468b271b8fa3c6f8ae
Dista <Dista@192.168.2.52>
2023-03-29 19:16:00 -0400

    finished filter.java main.java missing repo functionality

--- LaserfisheFileProcessor/src/main/java/Filter.java
+++ LaserfisheFileProcessor/src/main/java/Filter.java
@@ -2,6 +2,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  *
@@ -13,16 +14,62 @@ import java.util.Scanner;
 
 public class Filter {
     
-    String key;
-    File entry;
+    File[] entries;
     
-    
-    public Filter (String k, File f){
-        key = k;
-        entry = f;
+        public Filter (File f){
+        entries = f.listFiles();
     }
     
-    public String[] content() {
+    public String[] content(String key) {
         
+        ArrayList<String> output = new ArrayList<>();
+        
+        for(File entry : entries) {
+            try (Scanner scan = new Scanner(entry)) {
+                while (scan.hasNextLine()) {
+                    if (scan.nextLine().toLowerCase().contains(key)) {
+                        output.add(entry.getName());
+                        break;
+                    }
+                }
+                scan.close();
+            }
+            catch (Exception e1) {
+                System.out.println("There was an error with the file");
+            }
+            
+        }
+        
+        String[] foutput = output.toArray(String[]::new);
+        return foutput;
+    }
+    
+    public String[] count(String key, int min) {
+        
+        ArrayList<String> output = new ArrayList<>();
+        int count = 0;
+        
+        for(File entry : entries) {
+            
+            try (Scanner scan = new Scanner(entry)) {
+                while (scan.hasNextLine()) {
+                    if (scan.nextLine().toLowerCase().contains(key)) {
+                        count++;                       
+                    }
+                }
+                scan.close();
+            }
+            catch (Exception e1) {
+                System.out.println("There was an error with the file");
+            }
+            
+            if (count >= min) {
+                output.add(entry.getName());
+            }
+            
+        }
+        
+        String[] foutput = output.toArray(String[]::new);
+        return foutput;
     }
 }
--- LaserfisheFileProcessor/src/main/java/main.java
+++ LaserfisheFileProcessor/src/main/java/main.java
@@ -7,14 +7,30 @@ import java.io.File;
 
 public class main {
     public static void main(String[] args) {
-                      
+        
+        String key = "TORONTO";
+        key = key.toLowerCase();
+        int min = 2;
+
         try {
             File entry = new File("GeoNames");
+            Filter filter1 = new Filter(entry);
+            
+            System.out.println("Content Filtered:");
+            for (String i : filter1.content(key)){
+                System.out.println(i);
+            }
+            
+            System.out.println("Count Filtred:");
+            for (String i : filter1.count(key, min)){
+                System.out.println(i);
+            }
         }
         catch(Exception e) {
             System.out.println("The file is corrupt/ does not exist");
         }
-        
-        
+       
     }
 }
+
+
